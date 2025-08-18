368804b0a34486708b8671d684a888b8e71b8e01
Prateekagr3083 <prateekkhaitan38@gmail.com>
2025-08-18 09:57:11 +0000

    Aug 18, 2025, 3:27 PM

--- Navbar/Nave.php
+++ Navbar/Nave.php
@@ -3,22 +3,13 @@
         <div class="nav-logo">
             <a href="Home.php">Travel & Tours</a>
         </div>
-<ul class="nav-menu">
-    <li class="nav-item">
-        <a href="Home.php" class="nav-link active">Home</a>
-    </li>
-    <li class="nav-item">
-        <a href="About.php" class="nav-link">About</a>
-    </li>
-    <li class="nav-item">
-        <a href="Services.php" class="nav-link">Services</a>
-    </li>
-    <li class="nav-item">
-        <a href="Portfolio.php" class="nav-link">Portfolio</a>
-    </li>
-    <li class="nav-item">
-        <a href="Contact.php" class="nav-link">Contact</a>
-    </li>
-</ul>
+        <ul class="nav-menu">
+            <li class="nav-item">
+                <a href="Home.php" class="nav-link">Home</a>
+            </li>
+            <li class="nav-item">
+                <a href="Login.php" class="nav-link custom-btn btn-1">Login</a>
+            </li>
+        </ul>
     </div>
 </nav>
